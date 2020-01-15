7fcde8457056f3e78a500681ee1ecca29f48b8f4
unknown <ahmad.forhat@gmail.com>
2020-01-15 00:05:31 -0300

    Mudança no layout de envio do e-mail

--- Apoio/enviarEmail.js
+++ Apoio/enviarEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-const enviarEmail = (number, data) => {
+const enviarEmail = (number, data, dataMes, dataDia) => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth:{
@@ -8,32 +8,170 @@ const enviarEmail = (number, data) => {
             pass: process.env.pass
         }
     });
-    let array = []
-    let constructorHtml = ""
-    let charges = data.values[0]
+    let constructorAssessorMes = ""
+    let chargesMes = dataMes.values
 
-    for (i = 0; i< charges.length; i++){
-        constructorHtml += `<li><h3>${data.values[0][i]}</h3> ${data.values[number][i]}</li>`
+    for (i = 0; i< chargesMes.length; i++){
+        constructorAssessorMes += 
+        `<tr>
+        <td align="center" valign="top">${dataMes.values[i][0]}</td>
+        <td align="center" valign="top">${dataMes.values[i][1]}</td>
+        <td align="center" valign="top">${dataMes.values[i][2]}</td>
+        </tr>
+        `
+    }
+
+    let constructorAssessorDia = ""
+    let chargesDia = dataDia.values
+
+    for (i = 0; i< chargesDia.length; i++){
+        constructorAssessorDia += 
+        `<tr>
+        <td align="center" valign="top">${dataDia.values[i][0]}</td>
+        <td align="center" valign="top">${dataDia.values[i][1]}</td>
+        <td align="center" valign="top">${dataDia.values[i][2]}</td>
+        </tr>
+        `
     }
 
     // Options para mandar o e-mail
 
-    let hoje = new Date()
-    let month = hoje.getMonth()+1
-    let year = hoje.getFullYear()
+    function convertDate(inputFormat) {
+        function pad(s) { return (s < 10) ? '0' + s : s; }
+        var d = new Date(inputFormat)
+        return [pad(d.getDate()),pad(d.getMonth()+1),d.getFullYear()].join('/')
+      }
+    const date = new Date()
+    const dateHoje = convertDate(date.setDate(date.getDate()))
 
     let mailOptions = {
         from: 'ahmadziroteste@gmail.com',
         to: data.values[number][0],
-        subject: `KPI's ${data.values[number][1]}, ${hoje}`,
-        html: `<h2>KPI's de venda do dia ${hoje}</h2><ul>${constructorHtml}</ul><style>color:red</style>`
+        subject: `KPI's ${data.values[number][1]}, ${dateHoje}`,
+        html: 
+            `
+            <!DOCTYPE html>
+            <html lang="pt-br">
+            <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+            <body font-family: 'Roboto', sans-serif; margin:0;">
+            <table style="font-family: Arial" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
+            <tr>
+                <td align="center" valign="top">
+                    <table border="1" cellpadding="0" cellspacing="0" width="600" id="emailContainer">
+                        <tr>
+                            <td align="center" valign="top">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" id="emailHeader">
+                                    <tr>
+                                        <td align="center" valign="top">
+                                            <!-- THIS IS THE HEADER OF THE EMAIL -->
+                                            <div style="margin-bottom: 20px"></div>
+                                            <h2 style="display: inline; text-transform: uppercase; background: linear-gradient(transparent 60%, rgba(255,228,0,0.75) 100%)">ZIRO</h2>
+                                            <h3 style="text-transform: uppercase">Relatório Vendas</h3>
+                                            <p>24 de janeiro de 2020</p>
+                                            <div style="margin-bottom: 20px"></div>
+                                            <!-- END HEADER -->
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" valign="top">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" id="emailBody">
+                                    <tr>
+                                        <td align="center" valign="top">
+                                            <!-- THIS IS THE BODY OF THE EMAIL -->
+                                            <table width="100%">
+                                                <h4 style="padding: 20px 0; background: #222; color: #fff; text-transform: uppercase"><span style="color: #FFDD00; font-size: 20px">.&nbsp;</span>Comparativo Janeiro 2020 - 2019<span style="color: #FFDD00; font-size: 20px">&nbsp;.</span></h4>
+                                                <tbody>
+                                                    <tr>
+                                                        <td align="end" valign="top" style="padding-right: 20px"><b>Vendido 2020</b></td>
+                                                        <td align="start" valign="top" style="padding-left: 20px" width="50%">${data.values[number][2]}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="end" valign="top" style="padding-right: 20px"><b>Vendido 2019</b></td>
+                                                        <td align="start" valign="top" style="padding-left: 20px" width="50%">${data.values[number][3]}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="end" valign="top" style="padding-right: 20px"><b>Variação</b></td>
+                                                        <td align="start" valign="top" style="padding-left: 20px" width="50%">${data.values[number][4]}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="end" valign="top" style="padding-right: 20px"><b>Desempenho</b></td>
+                                                        <td align="start" valign="top" style="padding-left: 20px" width="50%">${data.values[number][5]}</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                            <table width="100%">
+                                                <h4 style="padding: 20px 0; background: #222; color: #fff; text-transform: uppercase"><span style="color: #FFDD00; font-size: 20px">.&nbsp;</span>Resultados acumulados<span style="color: #FFDD00; font-size: 20px">&nbsp;.</span></h4>
+                                                <thead>
+                                                    <tr>
+                                                        <th align="center" valign="top">Assessor</th>
+                                                        <th align="center" valign="top">Vendido</th>
+                                                        <th align="center" valign="top">Clientes</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${dataMes.values[0][3]}</td>
+                                                        <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${dataMes.values[1][3]}</td>
+                                                        <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${dataMes.values[2][3]}</td>
+                                                    </tr>
+                                                    ${constructorAssessorMes}
+                                                </tbody>
+                                            </table>
+                                            <table width="100%">
+                                                <h4 style="padding: 20px 0; background: #222; color: #fff; text-transform: uppercase"><span style="color: #FFDD00; font-size: 20px">.&nbsp;</span>Resultados do dia<span style="color: #FFDD00; font-size: 20px">&nbsp;.</span></h4>
+                                                <thead>
+                                                    <tr>
+                                                        <th align="center" valign="top">Assessor</th>
+                                                        <th align="center" valign="top">Vendido</th>
+                                                        <th align="center" valign="top">Clientes</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${dataDia.values[0][3]}</td>
+                                                        <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${dataDia.values[1][3]}</td>
+                                                        <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${dataDia.values[2][3]}</td>
+                                                    </tr>
+                                                    ${constructorAssessorDia}
+                                                </tbody>
+                                            </table>
+                                            <div style="margin-bottom: 20px"></div>
+                                            <!-- END BODY -->
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" valign="top">
+                                <table border="0" cellpadding="20" cellspacing="0" width="100%" id="emailFooter">
+                                    <tr>
+                                        <td align="center" valign="top">
+                                            <!-- THIS IS THE FOOTER OF THE EMAIL -->
+                                            <q>Do or do not. There is no try.</q>
+                                            <!-- END FOOTER -->
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>>
+            </body>
+            </html>
+            `
     };
 
     transporter.sendMail(mailOptions, function(err,data){
         if(err){
-            console.log('Aconteceu um erro')
+            console.log("Deu erro", err)
         }else{
-            console.log('Email foi enviado!')
+            console.log("Enviado com sucesso!")
         }
     })
 }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 //GoogleSheets GET
 
-async function main(){
+ async function main(){
 
     const url = "https://sheets.ziro.app/.netlify/functions/api"
     const username = process.env.userSheets
@@ -23,16 +23,53 @@ async function main(){
             "apiResource": "values",
             "apiMethod": "get",
             "spreadsheetId": process.env.sheetsId,
-            "range": "main!A1:Q3"
+            "range": "main!A1:M100"
+        },
+        json: true
+    }
+
+    let optionsGoogleAssessorMes = {
+        method: "GET",
+        url:url,
+        headers: {
+            "Origin": "https://ziro.app",
+            "Content-type": "application/json",
+            "Authorization": auth
+        },
+        body : {
+            "apiResource": "values",
+            "apiMethod": "get",
+            "spreadsheetId": process.env.sheetsId,
+            "range": "main!O3:R100"
+        },
+        json: true
+    }
+    let optionsGoogleAssessorDia = {
+        method: "GET",
+        url:url,
+        headers: {
+            "Origin": "https://ziro.app",
+            "Content-type": "application/json",
+            "Authorization": auth
+        },
+        body : {
+            "apiResource": "values",
+            "apiMethod": "get",
+            "spreadsheetId": process.env.sheetsId,
+            "range": "main!S3:V100"
         },
         json: true
     }
 
     try {
         let data = await rp(optionsGoogle)
-        
-        enviarEmail(1,data)
-
+        let dataMes = await rp(optionsGoogleAssessorMes)
+        let dataDia = await rp(optionsGoogleAssessorDia)
+        let i = data.values.length
+        while(i>1){
+            i--
+            enviarEmail(i,data, dataMes, dataDia)
+        }
     } catch (error) {
         console.log(error)
     }
