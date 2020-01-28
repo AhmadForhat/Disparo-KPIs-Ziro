be634c10d75419c50cde8eda73021444ccc1022c
unknown <ahmad.forhat@gmail.com>
2020-01-28 01:36:08 -0300

    setTimeOut funcionando

--- index.js
+++ index.js
@@ -14,29 +14,28 @@ require('dotenv').config()
                     const now = new Date();
                     const hora = now.getUTCHours()
                     const diaSemana = now.getUTCDay()
-                    if(hora == 22 && diaSemana != 6 && diaSemana != 0){
                         let i = arrayEmail.length
                         let funcoesPromise = []
                         while(i>0){
                             i--
                             funcoesPromise.push(enviarEmail(arrayEmail,i,data))
                         }
-                    }
                         console.log(await Promise.all(funcoesPromise))
                 }else{
+                    setTimeout(async () => {
                         const arrayEmail = (data.values[1][0]).split(",")
                         const now = new Date();
                         const hora = now.getUTCHours()
                         const diaSemana = now.getUTCDay()
-                        if(hora == 22 && diaSemana != 6 && diaSemana != 0){
                             let i = arrayEmail.length
                             let funcoesPromise = []
                             while(i>0){
                                 i--
                                 funcoesPromise.push(enviarEmail(arrayEmail,i,data))
                             }
-                        }
+                            console.log("SetTimeOut")
                             console.log(await Promise.all(funcoesPromise))
+                    }, 2000);
                 }
             } catch (error) {
                 console.log("Erro no disparo de email", error)
