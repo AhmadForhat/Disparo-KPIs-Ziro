6d3a37a8926ecb8e42d4ac1e2e82d3e5cbe79643
unknown <ahmad.forhat@gmail.com>
2020-01-30 23:17:24 -0300

    arrumando condicional para funcionar adequadamente

--- Apoio/util/corDesempenho.js
+++ Apoio/util/corDesempenho.js
@@ -1,11 +1,11 @@
 const corDesempenho = (data) => {
-    if(data.values[1][6] = "Abaixo da meta x2"){
+    if(data.values[1][6] == "Abaixo da meta x2"){
         return "color: #881600"
-    }if(data.values[1][6] = "Abaixo da Meta x2,5"){
+    }if(data.values[1][6] == "Abaixo da Meta x2,5"){
         return "color: #E53A40"
-    }if(data.values[1][6] = "Abaixo da Meta x3"){
+    }if(data.values[1][6] == "Abaixo da Meta x3"){
         return "color: #FFBC42"
-    }if(data.values[1][6] = "Meta atingida"){
+    }if(data.values[1][6] == "Meta atingida"){
         return "color: #5A9367"
     }else{
         return "color: black"
