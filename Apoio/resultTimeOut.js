589e0fcd3f81bae08eb85f2cedb04fa6b201b46a
unknown <ahmad.forhat@gmail.com>
2020-03-19 15:27:35 -0300

    generalizando as funcionalidades

--- Apoio/resultTimeOut.js
+++ Apoio/resultTimeOut.js
@@ -2,12 +2,26 @@ const retryHttp = require('./retryHttp')
 const rp = require('request-promise-native')
 const optionsGoogle = require('./solicitacaoSheets')
 const arrayObject = require('@ziro/array-object')
+const htmlVendas = require('./util/htmlVendas')
 
 const resulTimeOut = async () => {
-    const baseKpisVendas = await rp(optionsGoogle('main!A1:M2'))
+    // Requisições
+    const requests = await rp(optionsGoogle(['vendas!A1:M2']))
+    const [baseKpisVendas] = requests.valueRanges
     const kpisVendas = arrayObject(baseKpisVendas)
-    const result = await retryHttp(kpisVendas)
-    console.log(result)
+    //Vendas
+    // Funções de horários
+    const now = new Date()
+    const hora = now.getUTCHours()
+    const diaSemana = now.getUTCDay()
+    const condicionalVendas = hora == 22 && diaSemana != 6 && diaSemana != 0
+    // Condicional de loop
+    const vendaHoje = kpisVendas[0].assessores
+    const destinatariosVendas = kpisVendas[0].destinatarios
+    const condicionalLoopVendas = vendaHoje.startsWith('Total') && destinatariosVendas != ''
+    // Resultados
+    const resultVendas = await retryHttp(kpisVendas,htmlVendas,'Vendas',condicionalVendas,condicionalLoopVendas)
+    console.log(resultVendas)
 }
 
 module.exports = resulTimeOut
