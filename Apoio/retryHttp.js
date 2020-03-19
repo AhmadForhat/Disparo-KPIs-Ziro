93ddac22ecf64a52e8c9dd3f9ce013dca60034f2
unknown <ahmad.forhat@gmail.com>
2020-03-19 15:27:46 -0300

    generalizando as propriedades

--- Apoio/retryHttp.js
+++ Apoio/retryHttp.js
@@ -1,20 +1,15 @@
 const promiseVendas = require('./disparoFuncao')
-const html = require('./util/htmlVendas')
 
-const retryHttp = async (kpisVendas) => {
+const retryHttp = async (kpis,html,titulo,condicionalHorario,condicionalLoop) => {
     try {
-        const vendaHoje = kpisVendas[0].assessores
-        const destinatarios = kpisVendas[0].destinatarios
-        if(vendaHoje.startsWith('Total') && destinatarios != ''){
-            const arrayEmailVendas = destinatarios.split(',')
-            const now = new Date()
-            const hora = now.getUTCHours()
-            const diaSemana = now.getUTCDay()
-            if(hora == 22 && diaSemana != 6 && diaSemana != 0) return await promiseVendas(arrayEmailVendas,kpisVendas,html(kpisVendas[0]),'Vendas')
-        }else{
+        if(condicionalLoop){
+            const send = await promiseVendas(kpis,html(kpis[0]),titulo,condicionalHorario)
+            return send
+        }
+        else{
             console.log('Não ok!')
             return new Promise(resolve =>
-                setTimeout(() => resolve(retryHttp(kpisVendas)), 4000)
+                setTimeout(() => resolve(retryHttp(kpis)), 4000)
             )
         }
     } catch (error) {
