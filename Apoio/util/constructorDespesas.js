640ba6bb2b1f4057880152b5195d5decaa51b745
Ziro VI <ahmad.forhat@gmail.com>
2020-03-26 17:32:52 -0300

    arrumando condicional

--- Apoio/util/constructorDespesas.js
+++ Apoio/util/constructorDespesas.js
@@ -3,7 +3,7 @@ const constructorAssessor = (dadoAssessores) => {
         const arrayAssessor = dadoAssessores.split('#')
         let constructor = ''
         for (let i = 0; i< arrayAssessor.length; i += 4){
-            if(arrayAssessor[4] != 0){
+            if(true){
                 if(i == 0){
                     constructor += 
                 `<tr>
@@ -36,18 +36,7 @@ const constructorAssessor = (dadoAssessores) => {
             }
         }
         return constructor
-    }else{
-        const constructor = 
-    `<tr>
-    <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${arrayAssessor[0]}</td>
-    <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${arrayAssessor[1]}</td>
-    <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${arrayAssessor[2]}</td>
-    <td align="center" valign="top" style="border-bottom: 1px dotted rgba(0,0,0,0.1)">${arrayAssessor[3]}</td>
-    </tr>
-    `
-        console.log('Erro na split')
-        return constructor
-    }
+}
 }
 
 module.exports = constructorAssessor
